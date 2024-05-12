47b4125c0c7ef31074414480a967731c2b2f5037
Kinwailo <lokinwai@gmail.com>
2024-05-13 02:58:55 +0800

    fix ios icon

--- tools.sh
+++ tools.sh
@@ -1,16 +1,18 @@
 echo 1 flutter build windows
 echo 2 flutter build apk
-echo 3 flutter build web
-echo 4 dart run icons_launcher:create
-echo 5 dart run build_runner build
-echo 6 dart run package_rename_plus
+echo 3 flutter build appbundle
+echo 4 flutter build web
+echo 5 dart run icons_launcher:create
+echo 6 dart run build_runner build
+echo 7 dart run package_rename_plus
 
 read opt
 case $opt in
     1) flutter build windows;;
     2) flutter build apk;;
-    3) flutter build web;;
-    4) dart run icons_launcher:create;;
-    5) dart run build_runner build;;
-    6) dart run package_rename_plus;;
+    3) flutter build appbundle;;
+    4) flutter build web;;
+    5) dart run icons_launcher:create;;
+    6) dart run build_runner build;;
+    7) dart run package_rename_plus;;
 esac
