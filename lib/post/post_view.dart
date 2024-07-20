5538b9390484151b03b5342b8511c7ad32a321d9
Kinwailo <lokinwai@gmail.com>
2024-07-20 13:43:49 +0800

    decode the link to show correct words

--- lib/post/post_view.dart
+++ lib/post/post_view.dart
@@ -6,7 +6,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:linkify/linkify.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:visibility_detector/visibility_detector.dart';
@@ -761,6 +761,24 @@ class PostShortReply extends ConsumerWidget {
   }
 }
 
+TextSpan _linkifyTextSpan(String text) {
+  return TextSpan(children: [
+    for (var e in linkify(text))
+      if (e is LinkableElement)
+        TextSpan(
+          text: Uri.decodeComponent(e.text),
+          style: const TextStyle(
+            color: Colors.blueAccent,
+            decoration: TextDecoration.underline,
+          ),
+          recognizer: TapGestureRecognizer()
+            ..onTap = () => launchUrlString(e.url),
+        )
+      else
+        TextSpan(text: e.text),
+  ]);
+}
+
 class CustomScrollBehavior extends MaterialScrollBehavior {
   const CustomScrollBehavior();
   @override
@@ -796,20 +814,15 @@ class PostBodyText extends HookConsumerWidget {
         WidgetSpan(child: PostState(data)),
         _senderTextSpan(context, data),
       ],
-      if (data.state.error)
+      if (data.state.error) ...[
         WidgetSpan(
             child: Padding(
           padding: const EdgeInsets.only(right: 4),
           child: Icon(Icons.error, size: 18, color: colorScheme.error),
         )),
-      LinkifySpan(
-          text: text,
-          style: data.state.error ? TextStyle(color: colorScheme.error) : null,
-          linkStyle: const TextStyle(
-            color: Colors.blueAccent,
-            decoration: TextDecoration.underline,
-          ),
-          onOpen: (link) => launchUrlString(link.url)),
+        TextSpan(text: text, style: TextStyle(color: colorScheme.error)),
+      ],
+      if (!data.state.error) _linkifyTextSpan(text),
       if (data.state.error) ...[
         const TextSpan(text: ' '),
         TextSpan(
@@ -819,7 +832,7 @@ class PostBodyText extends HookConsumerWidget {
             recognizer: TapGestureRecognizer()
               ..onTap = () => ref.read(postsLoader).retry()),
         const TextSpan(text: ' '),
-      ]
+      ],
     ]);
 
     return VisibilityDetector(
